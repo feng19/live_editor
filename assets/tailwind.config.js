52a9eecde85a7f04c64deb5dda292affa0f8906b
feng19 <feng_19@foxmail.com>
2022-10-28 11:22:32 +0800

    feat: add daisy ui

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -16,8 +16,13 @@ module.exports = {
       }
     },
   },
+  daisyui: {
+    themes: ["corporate"],
+  },
   plugins: [
     require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
     plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
     plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
     plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
